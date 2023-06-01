650b219252da98abf0322eb880b40681c8d26c3f
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-06-01 09:58:04 +0300

    Виджет отзывов

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,61 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section/Section';
+import Notification from './Notification/Notification';
 import styles from './App.module.css';
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
   const handleLeaveFeedback = option => {
-    switch (option) {
-      case 'good':
-        setGood(prevState => prevState + 1);
-        break;
-
-      case 'neutral':
-        setNeutral(prevState => prevState + 1);
-        break;
-
-      case 'bad':
-        setBad(prevState => prevState + 1);
-        break;
-
-      default:
-        break;
-    }
+    setFeedback(prevState => ({
+      ...prevState,
+      [option]: prevState[option] + 1,
+    }));
   };
 
   const handleResetFeedback = () => {
-    setGood(0);
-    setNeutral(0);
-    setBad(0);
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
   };
 
   const countTotalFeedback = () => {
-    return good + neutral + bad;
+    return feedback.good + feedback.neutral + feedback.bad;
   };
 
   const countPositiveFeedbackPercentage = () => {
     const total = countTotalFeedback();
-    return total === 0 ? 0 : Math.round((good / total) * 100);
+    return total === 0 ? 0 : Math.round((feedback.good / total) * 100);
   };
+
+  const { good, neutral, bad } = feedback;
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
 
   return (
     <div className={styles.app}>
       <Section title="Please leave feedback">
         <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
+          options={Object.keys(feedback)}
           onLeaveFeedback={handleLeaveFeedback}
         />
       </Section>
 
-      {countTotalFeedback() > 0 && (
+      {total > 0 ? (
         <Section title="Statistics">
           <Statistics
             good={good}
             neutral={neutral}
             bad={bad}
-            total={countTotalFeedback()}
-            positivePercentage={countPositiveFeedbackPercentage()}
+            total={total}
+            positivePercentage={positivePercentage}
           />
           <button className={styles.resetButton} onClick={handleResetFeedback}>
             Reset Statistics
           </button>
         </Section>
+      ) : (
+        <Notification message="There is no feedback" />
       )}
     </div>
   );
